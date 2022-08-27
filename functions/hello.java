1843ca936b7c3e7ca4facc9dd5084e0956e1e246
Abhimanyu <abhimanyuajudiya.com>
2022-08-27 11:17:40 +0530

    new programs

---
+++ functions/hello.java
@@ -0,0 +1,12 @@
+public class hello {
+    public static void helloWorld() {
+        System.out.println("Hi!");
+        System.out.println("Hi!");
+        System.out.println("Hi!");
+        System.out.println("Hi!");
+        return;
+    }
+    public static void main(String[] args) {
+        helloWorld();
+    }
+}
