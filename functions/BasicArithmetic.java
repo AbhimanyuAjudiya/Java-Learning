7c724d670f76c64a90d488f5cd23277d44ad9368
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2025-11-27 10:41:28 +0530

    added subtraction feature in  BasicArithmetic.java file

--- functions/BasicArithmetic.java
+++ functions/BasicArithmetic.java
@@ -8,6 +8,10 @@ public class BasicArithmetic {
         int sum = number1 + number2;
         System.out.println("Addition: " + number1 + " + " + number2 + " = " + sum);
 
+        int difference = number1 - number2;
+        System.out.println("Subtraction: " + number1 + " - " + number2 + " = " + difference);
+
         System.out.println("Direct Addition: " + (50 + 20));
+        System.out.println("Direct Subtraction: " + (75 - 30));
     }
 }
