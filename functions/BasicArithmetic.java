9daa143be9f7e809e6d2a5421d2f5d1f4b5901c6
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2025-11-27 10:41:02 +0530

    added addition feature in  BasicArithmetic.java file

--- functions/BasicArithmetic.java
+++ functions/BasicArithmetic.java
@@ -2,5 +2,12 @@ public class BasicArithmetic {
 
     public static void main(String[] args) {
 
+        int number1 = 25;
+        int number2 = 10;
+
+        int sum = number1 + number2;
+        System.out.println("Addition: " + number1 + " + " + number2 + " = " + sum);
+
+        System.out.println("Direct Addition: " + (50 + 20));
     }
 }
